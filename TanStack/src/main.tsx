075f64c24d61d0d59693ed22f07605b8cecf2df2
jphani787 <jphanindra787@gmail.com>
2026-06-20 23:06:13 +0530

    tanstack setup

---
+++ TanStack/src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+const clientQuery = new QueryClient();
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={clientQuery}>
+    <App />
+    </QueryClientProvider>
+  </StrictMode>,
+)
