751fbc1caefe64fe7595230b0c24905361a69c44
jphani787 <jphanindra787@gmail.com>
2026-06-24 01:46:32 +0530

    updated modal

--- compound-components-design-pattern/src/with-pattren/modal/Modal.jsx
+++ compound-components-design-pattern/src/with-pattren/modal/Modal.jsx
@@ -1,24 +1,24 @@
-const Modal = ({ children }) => {
+const Modal = ({ children, isOpen, onClose }) => {
+    if (!isOpen) return null;
     return (
         <div className="modal-backdrop">
-            <div className="modal-container">
-
-            </div>
+            <div className="modal-container">{children}</div>
+            <button className="modal-close" onClick={onClose}>X</button>
         </div>
-    )
-}
+    );
+};
 
 const ModalHeader = ({ children }) => {
-    return <div className="modal-title">{children}</div>
-}
+    return <div className="modal-title">{children}</div>;
+};
 
 const ModalBody = ({ children }) => {
-    return <div className="modal-body">{children}</div>
-}
+    return <div className="modal-body">{children}</div>;
+};
 
 const ModalFooter = ({ children }) => {
-    return <div className="modal-footer">{children}</div>
-}
+    return <div className="modal-footer">{children}</div>;
+};
 
 Modal.Header = ModalHeader;
 Modal.Body = ModalBody;
